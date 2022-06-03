aa679926d93381a465aa37fc3c916c5f2de58460
kae-ya <19205@toba-cmt.ac.jp>
2022-06-03 12:23:12 +0900

    上村 script wip

--- JavaScript/movie_script.js
+++ JavaScript/movie_script.js
@@ -1,10 +1,19 @@
 //担当：上村
 //参考　https://qiita.com/iiishokoiii/items/3037d6d01248502aee68
 
-var v = document.getElementById('video');
-//再生
-v.play();
-//一時停止
-v.pause();
-//ロード
-v.load();
+window.onload = function() {
+  // 最初に動画を表示する。
+  var v = document.getElementById('video');
+    //再生
+    v.play();
+    //一時停止
+    v.pause();
+    //ロード
+    v.load();
+  question.innerHTML = //動画のパスを書いたら表示されるのか？
+};
+
+// クリック時の答え判定
+function hantei(btnNo) {
+  //btnNoの中にどちらの選手が取ったかの情報が入る
+  }
