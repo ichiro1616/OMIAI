505a89ee69c2d27bf08bc57e99de3c9f357281f2
ichiro1616 <ichiro.nakagawa1616@gmail.com>
2022-06-22 15:04:59 +0900

    Revert "6/22"
    This reverts commit ae68b7aefcb66bfdc45b20c601f34fdacd10d339.

--- Python/coord_transformation.py
+++ Python/coord_transformation.py
@@ -40,38 +40,14 @@ def transformation(csv, filename):
     # Cy2 = 1080 - 527.98858416498570
 
     # tz1 = 11.0 #7.35
-    tz1 = 6.25 #7.35
+    tz1 = 7.25 #7.35
 
     tz2 = 14.7
     csv['raw_x'] = 0.
     csv['raw_y'] = 0.
     csv['raw_z'] = 0.
-    csv['offset_x'] = 0.
-    csv['offset_z'] = 0.
-    csv['court_x'] = 0.
-    csv['court_z'] = 0.
-
-    offset_x = 0
-    offset_z = 0
 
     for index, row in csv.iterrows():
-        
-        if index == 0:
-            A = np.array([[-fx1, 0., -Cx1 + row['offset_usiro_x']],
-            [Cy2 - row['offset_yoko_y'], fy1 + fy2, -Cy1 + row['offset_usiro_y']],
-            [Cx2 - row['offset_yoko_x'], 0., -fx2]])
-            b = np.array([(-row['offset_usiro_x'] + Cx1) * tz1, (-row['offset_usiro_y'] + Cy1) * tz1 + (row['offset_yoko_y'] - Cy2) * tz2, (row['offset_yoko_x'] - Cx2) * tz2])    ##横、後ろカメラでもできるように、この辺の計算式をいじくる
-            x = np.linalg.solve(A, b)
-            x = x * np.array([1, -1, -1]) #ワールド座標の定義をxそのまま、y上向き、z前向きに変換する
-            offset_x = x[0]
-            offset_z = x[2]
-            csv.at[0, 'offset_x'] = offset_x 
-            csv.at[0, 'offset_z'] = offset_z
-            print(offset_x, offset_z)
-       
-
-
-
         # A = np.array([[fx1, 0., Cx1 - row['mae_x']],
         #     [Cy2 - row['yoko_y'], fy1 + fy2, Cy1 - row['mae_y']],
         #     [Cx2 - row['yoko_x'], 0., -fx2]])
@@ -94,16 +70,13 @@ def transformation(csv, filename):
         csv.at[index, 'raw_y'] = x[1]
         csv.at[index, 'raw_z'] = x[2]
 
-        csv.at[index, 'court_x'] = x[0] - offset_x + 4.5 
-        csv.at[index, 'court_z'] = x[2] - offset_z + 4
-
     # csv['mae_x'] = csv['mae_x'] / 1920
     # csv['mae_y'] = csv['mae_y'] / 1080
     # csv['usiro_x'] = csv['usiro_x']
     # csv['usiro_y'] = csv['usiro_y']
     # csv['yoko_x'] = csv['yoko_x']
     # csv['yoko_y'] = csv['yoko_y'] 
-    csv.to_csv('trans_' + filename, index=False) #,encoding='utf_8_sig'
+    csv.to_csv('transs_' + filename, index=False) #,encoding='utf_8_sig'
 
 
 if __name__ == '__main__':
