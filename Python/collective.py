a29d68575c4edbcf7f045111fb7e796d0aa72345
kae-ya <19205@toba-cmt.ac.jp>
2022-06-28 10:55:16 +0900

    sql uemura

--- Python/collective.py
+++ Python/collective.py
@@ -15,7 +15,6 @@ conn = MySQLdb.connect(
     db='omiai_db')
 cur = conn.cursor()
 
-
 #ローテーション別にテーブルから情報を抽出する
 cur.execute("SELECT * FROM `register` WHERE `rotation` = 1;")
 data1 = cur.fetchall()
@@ -31,6 +30,7 @@ cur.execute("SELECT * FROM `register` WHERE `rotation` = 6;")
 data6 = cur.fetchall()
 conn.commit() #結果を保存・確定する
 
+
 #経験年数別にデータを分類する
 #ローテーション：1
 data1_ex0 = []
@@ -38,17 +38,18 @@ data1_ex1 = []
 data1_ex2 = []
 data1_ex3 = []
 data1_ex4 = []
-for counter in data1:
-    if(data1[counter][experience_years] == 0):
-        data1_ex0 = data1[counter]
-    if(data1[counter][experience_years] == 1):
-        data1_ex1 = data1[counter]
-    if(data1[counter][experience_years] == 2):
-        data1_ex2 = data1[counter]
-    if(data1[counter][experience_years] == 3):
-        data1_ex3 = data1[counter]
-    if(data1[counter][experience_years] == 4):
-        data1_ex4 = data1[counter]
+for counter1 in data1:
+    if(data1[counter1][experience_years] == 0):
+        data1_ex0 = data1[counter1]
+        print(data1_ex0)
+    if(data1[counter1][experience_years] == 1):
+        data1_ex1 = data1[counter1]
+    if(data1[counter1][experience_years] == 2):
+        data1_ex2 = data1[counter1]
+    if(data1[counter1][experience_years] == 3):
+        data1_ex3 = data1[counter1]
+    if(data1[counter1][experience_years] == 4):
+        data1_ex4 = data1[counter1]
 
 #ローテーション：2
 data2_ex0 = []
@@ -56,17 +57,17 @@ data2_ex1 = []
 data2_ex2 = []
 data2_ex3 = []
 data2_ex4 = []
-for counter in data2:
-    if(data2[counter][experience_years] == 0):
-        data2_ex0 = data2[counter]
-    if(data2[counter][experience_years] == 1):
-        data2_ex1 = data2[counter]
-    if(data2[counter][experience_years] == 2):
-        data2_ex2 = data2[counter]
-    if(data2[counter][experience_years] == 3):
-        data2_ex3 = data2[counter]
-    if(data2[counter][experience_years] == 4):
-        data2_ex4 = data2[counter]
+for counter2 in data2:
+    if(data2[counter2][experience_years] == 0):
+        data2_ex0 = data2[counter2]
+    if(data2[counter2][experience_years] == 1):
+        data2_ex1 = data2[counter2]
+    if(data2[counter2][experience_years] == 2):
+        data2_ex2 = data2[counter2]
+    if(data2[counter2][experience_years] == 3):
+        data2_ex3 = data2[counter2]
+    if(data2[counter2][experience_years] == 4):
+        data2_ex4 = data2[counter2]
 
 #ローテーション：3
 data3_ex0 = []
@@ -74,17 +75,17 @@ data3_ex1 = []
 data3_ex2 = []
 data3_ex3 = []
 data3_ex4 = []
-for counter in data3:
-    if(data3[counter][experience_years] == 0):
-        data3_ex0 = data3[counter]
-    if(data3[counter][experience_years] == 1):
-        data3_ex1 = data3[counter]
-    if(data3[counter][experience_years] == 2):
-        data3_ex2 = data3[counter]
-    if(data3[counter][experience_years] == 3):
-        data3_ex3 = data3[counter]
-    if(data3[counter][experience_years] == 4):
-        data3_ex4 = data3[counter]
+for counter3 in data3:
+    if(data3[counter3][experience_years] == 0):
+        data3_ex0 = data3[counter3]
+    if(data3[counter3][experience_years] == 1):
+        data3_ex1 = data3[counter3]
+    if(data3[counter3][experience_years] == 2):
+        data3_ex2 = data3[counter3]
+    if(data3[counter3][experience_years] == 3):
+        data3_ex3 = data3[counter3]
+    if(data3[counter3][experience_years] == 4):
+        data3_ex4 = data3[counter3]
 
 #ローテーション：4
 data4_ex0 = []
@@ -92,17 +93,17 @@ data4_ex1 = []
 data4_ex2 = []
 data4_ex3 = []
 data4_ex4 = []
-for counter in data4:
-    if(data4[counter][experience_years] == 0):
-        data4_ex0 = data4[counter]
-    if(data4[counter][experience_years] == 1):
-        data4_ex1 = data4[counter]
-    if(data4[counter][experience_years] == 2):
-        data4_ex2 = data4[counter]
-    if(data4[counter][experience_years] == 3):
-        data4_ex3 = data4[counter]
-    if(data4[counter][experience_years] == 4):
-        data4_ex4 = data4[counter]
+for counter4 in data4:
+    if(data4[counter4][experience_years] == 0):
+        data4_ex0 = data4[counter4]
+    if(data4[counter4][experience_years] == 1):
+        data4_ex1 = data4[counter4]
+    if(data4[counter4][experience_years] == 2):
+        data4_ex2 = data4[counter4]
+    if(data4[counter4][experience_years] == 3):
+        data4_ex3 = data4[counter4]
+    if(data4[counter4][experience_years] == 4):
+        data4_ex4 = data4[counter4]
 
 #ローテーション：5
 data5_ex0 = []
@@ -110,17 +111,17 @@ data5_ex1 = []
 data5_ex2 = []
 data5_ex3 = []
 data5_ex4 = []
-for counter in data5:
-    if(data5[counter][experience_years] == 0):
-        data5_ex0 = data5[counter]
-    if(data5[counter][experience_years] == 1):
-        data5_ex1 = data5[counter]
-    if(data5[counter][experience_years] == 2):
-        data5_ex2 = data5[counter]
-    if(data5[counter][experience_years] == 3):
-        data5_ex3 = data5[counter]
-    if(data5[counter][experience_years] == 4):
-        data5_ex4 = data5[counter]
+for counter5 in data5:
+    if(data5[counter5][experience_years] == 0):
+        data5_ex0 = data5[counter5]
+    if(data5[counter5][experience_years] == 1):
+        data5_ex1 = data5[counter5]
+    if(data5[counter5][experience_years] == 2):
+        data5_ex2 = data5[counter5]
+    if(data5[counter5][experience_years] == 3):
+        data5_ex3 = data5[counter5]
+    if(data5[counter5][experience_years] == 4):
+        data5_ex4 = data5[counter5]
 
 #ローテーション：6
 data6_ex0 = []
@@ -128,17 +129,17 @@ data6_ex1 = []
 data6_ex2 = []
 data6_ex3 = []
 data6_ex4 = []
-for counter in data6:
-    if(data6[counter][experience_years] == 0):
-        data6_ex0 = data6[counter]
-    if(data6[counter][experience_years] == 1):
-        data6_ex1 = data6[counter]
-    if(data6[counter][experience_years] == 2):
-        data6_ex2 = data6[counter]
-    if(data6[counter][experience_years] == 3):
-        data6_ex3 = data6[counter]
-    if(data6[counter][experience_years] == 4):
-        data6_ex4 = data6[counter]
+for counter6 in data6:
+    if(data6[counter6][experience_years] == 0):
+        data6_ex0 = data6[counter6]
+    if(data6[counter6][experience_years] == 1):
+        data6_ex1 = data6[counter6]
+    if(data6[counter6][experience_years] == 2):
+        data6_ex2 = data6[counter6]
+    if(data6[counter6][experience_years] == 3):
+        data6_ex3 = data6[counter6]
+    if(data6[counter6][experience_years] == 4):
+        data6_ex4 = data6[counter6]
 
 
 
