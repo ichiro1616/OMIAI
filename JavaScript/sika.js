5bbe2e1b78b6cfed530e5a0e4662a81d811c81b3
yamato0502 <19206@toba-cmt.ac.jp>
2022-07-06 17:52:08 +0900

    コマの画像が変化、下の各ローテーションのコマが表示されるように修正

--- JavaScript/sika.js
+++ JavaScript/sika.js
@@ -322,61 +322,100 @@ let imagearray = [
 const canvas = document.getElementById('canvas3');
 const context = canvas.getContext('2d');
 
-//canvasに描画する準備
-const under_canvas = document.getElementById('under_canvas');
+//under_canvasに描画する準備
+const under_canvas = document.getElementById('under_canvas');//1ローテーション目
 const under_context = under_canvas.getContext('2d');
+const under_canvas2 = document.getElementById('under_canvas2');//2ローテーション目
+const under_context2 = under_canvas2.getContext('2d');
+const under_canvas3 = document.getElementById('under_canvas3');//3ローテーション目
+const under_context3 = under_canvas3.getContext('2d');
+const under_canvas4 = document.getElementById('under_canvas4');//4ローテーション目
+const under_context4 = under_canvas4.getContext('2d');
+const under_canvas5 = document.getElementById('under_canvas5');//5ローテーション目
+const under_context5 = under_canvas5.getContext('2d');
+const under_canvas6 = document.getElementById('under_canvas6');//6ローテーション目
+const under_context6 = under_canvas6.getContext('2d');
 
 
 //画像パス
 const srcs = [
-    '../Picture/コマ_透過ながい.png',
-    '../Picture/コマ_透過ながい.png',
-    '../Picture/コマ_透過ながい.png',
-    '../Picture/コマ_透過ながい.png',
-    '../Picture/コマ_透過ながい.png',
-    '../Picture/コマ_透過ながい.png',
+    //ローテーション0
+    [
+        '../Picture/koma/0/2-1.png',//セッター（コマ0）前衛
+        '../Picture/koma/1/front/2-2.png',//るい（コマ1）前衛
+        '../Picture/koma/2/front/2-3.png',//ひなた（コマ2）前衛
+        '../Picture/koma/3/back/2-4.png',//けんと（コマ3）後衛
+        '../Picture/koma/4/back/2-5.png',//りく（コマ4）後衛
+        '../Picture/koma/5/back/2-6.png',//けいすけ（コマ5）後衛
+    ],
+    //ローテーション1
+    [
+        '../Picture/koma/0/2-1.png',//セッター（コマ0）前衛
+        '../Picture/koma/1/front/2-2.png',//るい（コマ1）前衛
+        '../Picture/koma/2/back/2-3.png',//ひなた（コマ2）後衛
+        '../Picture/koma/3/back/2-4.png',//けんと（コマ3）後衛
+        '../Picture/koma/4/back/2-5.png',//りく（コマ4）後衛
+        '../Picture/koma/5/front/2-6.png',//けいすけ（コマ5）前衛
+    ],
+    //ローテーション2
+    [
+        '../Picture/koma/0/2-1.png',//セッター（コマ0）前衛
+        '../Picture/koma/1/back/2-2.png',//るい（コマ1）後衛
+        '../Picture/koma/2/back/2-3.png',//ひなた（コマ2）後衛
+        '../Picture/koma/3/back/2-4.png',//けんと（コマ3）後衛
+        '../Picture/koma/4/front/2-5.png',//りく（コマ4）前衛
+        '../Picture/koma/5/front/2-6.png',//けいすけ（コマ5）前衛
+    ],
+    //ローテーション3
+    [
+        '../Picture/koma/0/2-1.png',//セッター（コマ0）後衛
+        '../Picture/koma/1/back/2-2.png',//るい（コマ1）後衛
+        '../Picture/koma/2/back/2-3.png',//ひなた（コマ2）後衛
+        '../Picture/koma/3/front/2-4.png',//けんと（コマ3）前衛
+        '../Picture/koma/4/front/2-5.png',//りく（コマ4）前衛
+        '../Picture/koma/5/front/2-6.png',//けいすけ（コマ5）前衛
+    ],
+    //ローテーション4
+    [
+        '../Picture/koma/0/2-1.png',//セッター（コマ0）後衛
+        '../Picture/koma/1/back/2-2.png',//るい（コマ1）後衛
+        '../Picture/koma/2/front/2-3.png',//ひなた（コマ2）前衛
+        '../Picture/koma/3/front/2-4.png',//けんと（コマ3）前衛
+        '../Picture/koma/4/front/2-5.png',//りく（コマ4）前衛
+        '../Picture/koma/5/back/2-6.png',//けいすけ（コマ5）後衛
+    ],
+    //ローテーション5
+    [
+        '../Picture/koma/0/2-1.png',//セッター（コマ0）後衛
+        '../Picture/koma/1/front/2-2.png',//るい（コマ1）前衛
+        '../Picture/koma/2/front/2-3.png',//ひなた（コマ2）前衛
+        '../Picture/koma/3/front/2-4.png',//けんと（コマ3）前衛
+        '../Picture/koma/4/back/2-5.png',//りく（コマ4）後衛
+        '../Picture/koma/5/back/2-6.png',//けいすけ（コマ5）後衛
+    ]
 ];
 
 //画像 ローテーションするなら多次元にした方がいいかも
-let images = [];
-for (var i in srcs) {
-    images[i] = new Image();
+let images = new Array(6);//要素数6の配列imagesを作成
+for (var i = 0; i < 6; i++) {
+    images[i] = new Array(6).fill(0);//imagesの多次元配列を作成し0で初期化
+}
+for (var i = 0; i < 6; i++) {//ローテーション
+    for (var j = 0; j < 6; j++) {//コマ番号
+        images[i][j] = new Image();
+    }
 }
 
-
-let dragmode = true;//ドラッグモード
+let scale = under_canvas.width / canvas.width;//canvasとunder_canvasの比
+let dragmode = false;//ドラッグモード
 let dragkoma = null;//ドラッグするコマの添え字
-let size = 2.5;//コマの大きさの倍率
-let koma_w = 130;//コマの横幅
+let size = 2.0;//メイン画面のコマの大きさの倍率
+let size_under = scale * size;//サブ画面のコマの大きさの倍率
+let koma_w = 100;//コマの横幅
 let koma_h = 100;//コマの高さ
 let counter = 0;//ローテーションカウント用
-
-window.addEventListener('DOMContentLoaded', () => {
-    //初期座標にコマを表示させる
-    images[0].addEventListener('load', () => {
-        context.drawImage(images[0], imagearray[0][0].x, imagearray[0][0].y, koma_w * size, koma_h * size);
-    })
-    images[1].addEventListener('load', () => {
-        context.drawImage(images[1], imagearray[0][1].x, imagearray[0][1].y, koma_w * size, koma_h * size);
-    })
-    images[2].addEventListener('load', () => {
-        context.drawImage(images[2], imagearray[0][2].x, imagearray[0][2].y, koma_w * size, koma_h * size);
-    })
-    images[3].addEventListener('load', () => {
-        context.drawImage(images[3], imagearray[0][3].x, imagearray[0][3].y, koma_w * size, koma_h * size);
-    })
-    images[4].addEventListener('load', () => {
-        context.drawImage(images[4], imagearray[0][4].x, imagearray[0][4].y, koma_w * size, koma_h * size);
-    })
-    images[5].addEventListener('load', () => {
-        context.drawImage(images[5], imagearray[0][5].x, imagearray[0][5].y, koma_w * size, koma_h * size);
-    })
-
-    //画像読み込み終わってからソース取得する
-    for (var i in srcs) {
-        images[i].src = srcs[i];
-    }
-});
+let startX;//ドラッグ開始時のx座標
+let startY;//ドラッグ開始時のy座標
 
 //コマの中心の座標を用意
 for (var i in imagearray) {
@@ -386,11 +425,90 @@ for (var i in imagearray) {
     }
 }
 
+//セッターの座標を修正
+imagearray_center[3][0].x = imagearray_center[3][5].x + koma_w / 2 * size * Math.cos(Math.PI / 4) - 50 + 10 * size;//45°
+imagearray_center[3][0].y = imagearray_center[3][5].y + koma_h / 2 * size * Math.sin(Math.PI / 4) - 50 + 10 * size;//45°
+imagearray[3][0].x += koma_w / 2 * size * Math.cos(Math.PI / 4) - 50 + 10 * size;//45°
+imagearray[3][0].y += koma_h / 2 * size * Math.sin(Math.PI / 4) - 50 + 10 * size;//45°
+
+imagearray_center[4][0].x = imagearray_center[4][3].x - koma_w / 2 * size * Math.cos(Math.PI / 4) + 50 - 10 * size;//45°
+imagearray_center[4][0].y = imagearray_center[4][3].y + koma_h / 2 * size * Math.sin(Math.PI / 4) + 50 - 10 * size;//45°
+imagearray[4][0].x -= koma_w / 2 * size * Math.cos(Math.PI / 4) - 50 + 10 * size;//45°
+imagearray[4][0].y += koma_h / 2 * size * Math.sin(Math.PI / 4) - 50 + 10 * size;//45°
+
+imagearray_center[5][0].x = imagearray_center[5][1].x - koma_w / 2 * size * Math.cos(Math.PI / 4) - 50 + 10 * size;//45°
+imagearray_center[5][0].y = imagearray_center[5][1].y + koma_h / 2 * size * Math.sin(Math.PI / 4) - 50 + 10 * size;//45°
+imagearray[5][0].x -= koma_w / 2 * size * Math.cos(Math.PI / 4) - 50 + 10 * size;//45°
+imagearray[5][0].y += koma_h / 2 * size * Math.sin(Math.PI / 4) - 50 + 10 * size;//45°
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    //初期座標にコマを表示させる
+    for (let i = 0; i < 6; i++) {
+        images[0][i].addEventListener('load', () => {
+            context.drawImage(images[0][i], imagearray[0][i].x, imagearray[0][i].y, koma_w * size, koma_h * size)
+        })
+        switch (i) {
+            case 0://1ローテーション目
+                for (let j = 0; j < 6; j++) {
+                    images[i][j].addEventListener('load', () => {
+                        under_context.drawImage(images[i][j], imagearray[i][j].x * scale, imagearray[i][j].y * scale, koma_w * size_under, koma_h * size_under);
+                    })
+                }
+                break;
+            case 1://2ローテーション
+                for (let j = 0; j < 6; j++) {
+                    images[i][j].addEventListener('load', () => {
+                        under_context2.drawImage(images[i][j], imagearray[i][j].x * scale, imagearray[i][j].y * scale, koma_w * size_under, koma_h * size_under);
+                    })
+                }
+                break;
+            case 2://3ローテーション目
+                for (let j = 0; j < 6; j++) {
+                    images[i][j].addEventListener('load', () => {
+                        under_context3.drawImage(images[i][j], imagearray[i][j].x * scale, imagearray[i][j].y * scale, koma_w * size_under, koma_h * size_under);
+                    })
+                }
+                break;
+            case 3://4ローテーション
+                for (let j = 0; j < 6; j++) {
+                    images[i][j].addEventListener('load', () => {
+                        under_context4.drawImage(images[i][j], imagearray[i][j].x * scale, imagearray[i][j].y * scale, koma_w * size_under, koma_h * size_under);
+                    })
+                }
+                break;
+            case 4://5ローテーション
+                for (let j = 0; j < 6; j++) {
+                    images[i][j].addEventListener('load', () => {
+                        under_context5.drawImage(images[i][j], imagearray[i][j].x * scale, imagearray[i][j].y * scale, koma_w * size_under, koma_h * size_under);
+                    })
+                }
+                break;
+            case 5://6ローテーション
+                for (let j = 0; j < 6; j++) {
+                    images[i][j].addEventListener('load', () => {
+                        under_context6.drawImage(images[i][j], imagearray[i][j].x * scale, imagearray[i][j].y * scale, koma_w * size_under, koma_h * size_under);
+                    })
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    //画像読み込み終わってからソース取得する
+    for (var i = 0; i < 6; i++) {
+        for (var j = 0; j < 6; j++) {
+            images[i][j].src = srcs[i][j];
+        }
+    }
+});
+
+
 //画像を表示する
 function draw(rota) {
     // canvas内を一旦クリア
     context.clearRect(0, 0, canvas.width, canvas.height);
-    under_context.clearRect(0, 0, under_canvas.width, under_canvas.height);
 
     for (var i in images) {
         let x = imagearray[rota][i].x;
@@ -398,8 +516,7 @@ function draw(rota) {
         let w = koma_w * size;
         let h = koma_h * size;
 
-        context.drawImage(images[i], x, y, w, h);
-        under_context.drawImage(images[i], x, y, w, h);
+        context.drawImage(images[rota][i], x, y, w, h);
     }
 }
 
@@ -435,24 +552,24 @@ let mousedown = function (e) {
         }
     }
 
-    if (dragkoma == 0) {
-        document.getElementById("syoukai_bun").innerHTML = "安西";
-    }
-    if (dragkoma == 1) {
-        document.getElementById("syoukai_bun").innerHTML = "けんと";
-    }
-    if (dragkoma == 2) {
-        document.getElementById("syoukai_bun").innerHTML = "りく";
-    }
-    if (dragkoma == 3) {
-        document.getElementById("syoukai_bun").innerHTML = "いちろ";
-    }
-    if (dragkoma == 4) {
-        document.getElementById("syoukai_bun").innerHTML = "けいすけ";
-    }
-    if (dragkoma == 5) {
-        document.getElementById("syoukai_bun").innerHTML = "ひなた";
-    }
+    // if (dragkoma == 0) {
+    //     document.getElementById("syoukai_bun").innerHTML = "安西";
+    // }
+    // if (dragkoma == 1) {
+    //     document.getElementById("syoukai_bun").innerHTML = "けんと";
+    // }
+    // if (dragkoma == 2) {
+    //     document.getElementById("syoukai_bun").innerHTML = "りく";
+    // }
+    // if (dragkoma == 3) {
+    //     document.getElementById("syoukai_bun").innerHTML = "いちろ";
+    // }
+    // if (dragkoma == 4) {
+    //     document.getElementById("syoukai_bun").innerHTML = "けいすけ";
+    // }
+    // if (dragkoma == 5) {
+    //     document.getElementById("syoukai_bun").innerHTML = "ひなた";
+    // }
 }
 
 //ドラッグ中処理
@@ -1116,7 +1233,53 @@ let mousemove = function (e) {
             }
 
             // 画像を描画
-            context.drawImage(images[i], x, y, w, h)
+            context.drawImage(images[counter][i], x, y, w, h);
+            switch (counter) {
+                case 0://1ローテーション目
+                    under_context.clearRect(0, 0, under_canvas.width, under_canvas.height);
+                    for (let j = 5; j >= 0; j--) {
+                        under_context.drawImage(images[counter][j], imagearray[counter][j].x * scale, imagearray[counter][j].y * scale, koma_w * size_under, koma_h * size_under);
+                    }
+                    break;
+                case 1://2ローテーション目
+                    under_context2.clearRect(0, 0, under_canvas2.width, under_canvas2.height);
+                    for (let j = 5; j >= 0; j--) {
+                        under_context2.drawImage(images[counter][j], imagearray[counter][j].x * scale, imagearray[counter][j].y * scale, koma_w * size_under, koma_h * size_under);
+                    }
+                    break;
+                case 2://3ローテーション目
+                    under_context3.clearRect(0, 0, under_canvas3.width, under_canvas3.height);
+                    for (let j = 5; j >= 0; j--) {
+                        under_context3.drawImage(images[counter][j], imagearray[counter][j].x * scale, imagearray[counter][j].y * scale, koma_w * size_under, koma_h * size_under);
+                    }
+                    break;
+                case 3://4ローテーション目
+                    under_context4.clearRect(0, 0, under_canvas4.width, under_canvas4.height);
+                    for (let j = 5; j >= 0; j--) {
+                        under_context4.drawImage(images[counter][j], imagearray[counter][j].x * scale, imagearray[counter][j].y * scale, koma_w * size_under, koma_h * size_under);
+                    }
+                    break;
+                case 4://5ローテーション目
+                    under_context5.clearRect(0, 0, under_canvas5.width, under_canvas5.height);
+                    for (let j = 5; j >= 0; j--) {
+                        under_context5.drawImage(images[counter][j], imagearray[counter][j].x * scale, imagearray[counter][j].y * scale, koma_w * size_under, koma_h * size_under);
+                    }
+                    break;
+                case 5://6ローテーション目
+                    under_context6.clearRect(0, 0, under_canvas6.width, under_canvas6.height);
+                    for (let j = 5; j >= 0; j--) {
+                        under_context6.drawImage(images[counter][j], imagearray[counter][j].x * scale, imagearray[counter][j].y * scale, koma_w * size_under, koma_h * size_under);
+                    }
+                    break;
+                default:
+                    break;
+            }
+            if (counter == 0) {
+                under_context.clearRect(0, 0, under_canvas.width, under_canvas.height);
+                for (let j = 5; j >= 0; j--) {
+                    under_context.drawImage(images[0][j], imagearray[0][j].x * scale, imagearray[0][j].y * scale, koma_w * size_under, koma_h * size_under);
+                }
+            }
         }
     }
 };
