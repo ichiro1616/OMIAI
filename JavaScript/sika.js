7058bc0bb329a4281aab58846e13fbba5d57465c
yamato0502 <19206@toba-cmt.ac.jp>
2022-08-04 13:23:53 +0900

    一路の関数を追加（まだ未完成）

--- JavaScript/sika.js
+++ JavaScript/sika.js
@@ -631,25 +631,6 @@ let mousedown = function (e) {
             break;
         }
     }
-
-    // if (dragkoma == 0) {
-    //     document.getElementById("syoukai_bun").innerHTML = "安西";
-    // }
-    // if (dragkoma == 1) {
-    //     document.getElementById("syoukai_bun").innerHTML = "けんと";
-    // }
-    // if (dragkoma == 2) {
-    //     document.getElementById("syoukai_bun").innerHTML = "りく";
-    // }
-    // if (dragkoma == 3) {
-    //     document.getElementById("syoukai_bun").innerHTML = "いちろ";
-    // }
-    // if (dragkoma == 4) {
-    //     document.getElementById("syoukai_bun").innerHTML = "けいすけ";
-    // }
-    // if (dragkoma == 5) {
-    //     document.getElementById("syoukai_bun").innerHTML = "ひなた";
-    // }
 }
 
 //ドラッグ中処理
@@ -1490,11 +1471,12 @@ let mousemove = function (e) {
     }
 };
 
-
 //ドラッグ終了処理
 let mouseup = function (e) {
+    if (dragmode) {
+        area_calculation(counter);
+    }
     dragmode = false;
-    document.getElementById("syoukai_bun").innerHTML = "";
 }
 
 //コマ以外を押したとき
@@ -1517,7 +1499,6 @@ function rotation() {
     console.log("ローテーション", counter);
     draw(counter);
 }
-
 
 //登録ボタンを押したときの処理
 document.getElementById("register_btn").onclick = function () {
@@ -1544,5 +1525,234 @@ document.getElementById("register_btn").onclick = function () {
             xhr.send(formData);
         }
     }
+}
 
+let color_array = [];
+let test_data = [];
+let judge_color = [];
+
+//お見合い範囲の計算
+function area_calculation(rota) {
+    //lr.coef_の値取得、テストデータ作成、特徴量として変換、
+    let formData_area = new FormData();
+    let xhr_area = new XMLHttpRequest();
+    xhr_area.open("POST", "../PHP/sika.php");
+    xhr_area.addEventListener("loadend", function () {
+        if (xhr_area.status === 200) {
+            let data = JSON.parse(xhr_area.response);
+            console.log(data);
+            let color_array_data = [];
+            for (i = 0; i < data.length; i++) {
+                // data[i] = data[i].slice(1);
+                // data[i] = data[i].slice(0, -1);
+                color_array[i] = data[i].data.split(",");//データいれる
+            }
+            console.log('data', data[0].color);
+            console.log('data', data[0 + 85].color);
+            console.log('data', data[85 + 85].color);
+            console.log('first', color_array_data);
+            // console.log(color_array);
+            for (i = 0; i < color_array.length; i++) {
+                for (j = 0; j < color_array[i].length; j++) {
+                    color_array[i][j] = Number(color_array[i][j]);
+                }
+            }
+            // console.log('number', color_array[3][0]);
+            // console.log('color_array[0].length', color_array[0].length);
+            // console.log('color_array.length', color_array.length);
+
+            // for (i = 0; i < 3; i++) {
+            //     color_array[i] = new Array(85).fill(0);//多次元配列を作成し0で初期化
+            // }
+            // for (i = 0; i < 3; i++) {
+            //     switch (i) {
+            //         case 0:
+            //             for (j = 0; j < 85; j++) {
+            //                 color_array[i][j] = data[j].data;
+            //             }
+            //             break;
+            //         case 1:
+            //             for (j = 0; j < 85; j++) {
+            //                 color_array[i][j] = data[j + 85].data
+            //             }
+            //             break;
+            //         case 2:
+            //             for (j = 0; j < 85; j++) {
+            //                 color_array[i][j] = data[j + 85 + 85].data;
+            //             }
+            //             break;
+            //     }
+            // }
+            console.log('color_array', color_array);
+            //左の選手
+            let player1_x = imagearray_center[rota][1].x;
+            let player1_y = imagearray_center[rota][1].y;
+            //右の選手
+            let player2_x = imagearray_center[rota][2].x;
+            let player2_y = imagearray_center[rota][2].y;
+
+            for (i = 0; i < 9.2; i += 0.2) {
+                for (j = 0; j < 9.2; j += 0.2) {
+                    let data_tmp = {};
+                    data_tmp.players_sabun_x = Math.abs(player1_x - player2_x);
+                    data_tmp.players_sabun_y = Math.abs(player1_y - player2_y);
+                    data_tmp.player1_ball_sabun_x = Math.abs(player1_x - i);
+                    data_tmp.player1_ball_sabun_y = Math.abs(player1_y - j);
+                    data_tmp.player2_ball_sabun_x = Math.abs(player2_x - i);
+                    data_tmp.player2_ball_sabun_y = Math.abs(player2_y - j);
+                    test_data.push(data_tmp);
+
+                    let data_view = {};
+                    data_view.ball_x = i;
+                    data_view.ball_y = j;
+                    data_view.judge = 100;
+                    judge_color.push(data_view);
+                }
+            }
+
+            // console.log(test_data);
+            let blue = [];
+            let red = [];
+            let green = [];
+            let answer = [];
+
+            for (o = 0; o < test_data.length; o++) {
+                answer.push(
+                    sum(
+                        test_data[o].players_sabun_x,
+                        test_data[o].players_sabun_y,
+                        test_data[o].player1_ball_sabun_x,
+                        test_data[o].player1_ball_sabun_y,
+                        test_data[o].player2_ball_sabun_x,
+                        test_data[o].player2_ball_sabun_y
+                    )
+                );
+                // console.log("aa");
+            }
+            // console.log(answer);
+
+            array = [];
+            //列で正規化をしている
+            for (i = 0; i < answer[0].length; i++) {
+                for (j = 0; j < answer.length; j++) {
+                    array.push(answer[j][i]);
+                }
+                max = Math.max(...array);
+                min = Math.min(...array);
+                // console.log(array, "max", max, "min", min);
+
+                //NaNを0にしてる
+                for (k = 0; k < answer.length; k++) {
+                    if (isNaN((answer[k][i] - min) / (max - min))) {
+                        answer[k][i] = 0;
+                    } else {
+                        answer[k][i] = (answer[k][i] - min) / (max - min);
+                    }
+                }
+
+                array.length = 0;
+            }
+            // console.log(answer);
+            for (i = 0; i < answer.length; i++) {
+                //lr.intercept_の値を足している
+                let b = 0;
+                let r = 0;
+                let g = 0;
+
+                //lr.coef_の値とpolynomialの値をかける
+                for (j = 0; j < color_array[0].length - 1; j++) {
+                    // answer[i][j] = (answer[i][j] - min) / (max - min);
+                    // answer[i][j] = (answer[i][j] - min_array[i]) / (max_array[i] - min_array[i]);
+
+                    b = b + answer[i][j] * color_array[0][j];
+                    r = r + answer[i][j] * color_array[1][j];
+                    g = g + answer[i][j] * color_array[2][j];
+                    // console.log(j, b);
+                }
+                b = b + color_array[0][color_array[0].length - 1];
+                r = r + color_array[1][color_array[0].length - 1];
+                g = g + color_array[2][color_array[0].length - 1];
+                // console.log(b, r, g);
+
+                aa = 1 / (1 + Math.exp(-b));
+                bb = 1 / (1 + Math.exp(-r));
+                cc = 1 / (1 + Math.exp(-g));
+                blue.push(aa);
+                red.push(bb);
+                green.push(cc);
+                // console.log(i, b, r, g, aa, bb, cc);
+            }
+            // console.log(answer[0]);
+            // console.log(color_array[0]);
+            judge_array = [0, 0, 0];
+            for (i = 0; i < blue.length; i++) {
+                //どの色になるかの判断
+                judge = [blue[i], red[i], green[i]];
+                // console.log(judge);
+                // console.log(blue[i], red[i], green[i]);
+                judge_color[i].judge = judge.lastIndexOf(Math.max(...judge));
+                // console.log(judge_color[i].judge);
+                judge_array[judge_color[i].judge] += 1;
+                judge.length = 0;
+            }
+            console.log(judge_color);
+            console.log(judge_array);
+        }
+    });
+    xhr_area.send(formData_area);
+}
+
+function sum(x1, x2, x3, x4, x5, x6) {
+    //polynomialfeatureの3次元の乗算の組み合わせを表示
+    let array = [x1, x2, x3, x4, x5, x6];
+    let counter_k = -1;
+    let counter_j = 7;
+    let counter_i = -1;
+    let one = [];
+    let two = [];
+    let three = [];
+    let four = [];
+    let five = [];
+    let six = [];
+    let box = [one, two, three, four, five, six];
+    let answer = [1];
+    for (i = 0; i < array.length + 1; i++) {
+        for (j = 0; j < array.length + 1; j++) {
+            for (k = 0; k < array.length; k++) {
+                if (i == 0) {
+                    if (j == 0) {
+                        answer.push(array[k]);
+                    } else {
+                        if (isNaN(array[j - 1] * array[k + counter_k])) {
+                        } else {
+                            box[j - 1].push(array[j - 1] * array[k + counter_k]);
+                            answer.push(array[j - 1] * array[k + counter_k]);
+                        }
+                    }
+                } else {
+                    if (j == 0) {
+                    } else {
+                        if (k < counter_j) {
+                            if (j > counter_i) {
+                                if (isNaN(box[j - 1][k] * array[i - 1])) {
+                                } else {
+                                    // console.log("k", k, "j", j, "i", i, "answer", box[j - 1][k] * i, "counter_j", counter_j, "box[j-1]", box[j - 1]);
+                                    answer.push(box[j - 1][k] * array[i - 1]);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            counter_k++;
+            // console.log("counter", counter_k);
+        }
+        counter_j--;
+        counter_i++;
+        counter_k = -1;
+        // console.log("NOW", answer);
+    }
+    // console.log(answer);
+    // console.log(box);
+    return answer;
 }
---
+++ PHP/sika.php
@@ -0,0 +1,32 @@
+<?php
+// $id = $_POST['player_id'];
+$dsn = 'mysql:dbname=omiai_db;host=localhost';
+$user = 'root';
+$password = 'naramie';
+$result = array();
+// $result2 = array();
+
+try {
+$dbh = new PDO($dsn, $user, $password);
+
+// 最新の2550個のレコードを取得
+$sql = "SELECT * FROM `lr.coef_` ORDER BY `lr.coef_id` ASC LIMIT 255";
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+$result = $stmt->fetchAll();
+
+
+// 指定したid以外で、指定したball_idと同じball_idのid、名前などの値をランダムで取得
+// $sql2 = "SELECT * FROM `users` WHERE NOT (`id` = $id) AND `ball_id` = {$result[0]["ball_id"]} ORDER BY RAND() LIMIT 4";
+// $stmt2 = $dbh->prepare($sql2);
+// $stmt2->execute();
+// $result2 = $stmt2->fetchAll();
+// $result_arr = array($result, $result2);
+} catch (Exception$e) {
+echo$e->getMessage();
+exit();
+}
+
+header('Content-type: application/json');
+echo json_encode($result,JSON_UNESCAPED_UNICODE);
+?>
