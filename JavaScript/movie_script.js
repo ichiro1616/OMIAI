12d2b2b28edb9ab18278d4934196c9dbf919151d
kae-ya <19205@toba-cmt.ac.jp>
2022-08-04 17:36:17 +0900

    duration

--- JavaScript/movie_script.js
+++ JavaScript/movie_script.js
@@ -94,7 +94,7 @@ function movie_time() {
 
   videoElement = document.getElementById("mv");
   videoElement.addEventListener("timeupdate", function () {
-    submit = videoElement.currentTime;
+    submit = videoElement.duration;
     console.log(submit);
 
     if ((stop_time - submit) <= 0.01 && position == 0) {
