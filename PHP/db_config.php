adfa7126a48476e11d6e90d02bee04c21e8f045b
kae-ya <19205@toba-cmt.ac.jp>
2022-08-11 19:04:11 +0900

    db_comfig.py

--- PHP/db_config.php
+++ PHP/db_config.php
@@ -6,6 +6,6 @@ $password = 'Pa22wadoh';
 
 //ロリポップ
 // $dsn = 'mysql:dbname=LAA1281735-omiaidb;host=mysql205.phy.lolipop.lan';
-// $user = ' LAA1281735';
+// $user = 'LAA1281735';
 // $password = 'Pa22wadoh';
 ?>
---
+++ Python/db_config.py
@@ -0,0 +1,11 @@
+#ローカル
+host='localhost'
+user='root'
+passwd='Pa22wadoh'
+db='omiai_db'
+
+#ロリポップ
+# host='mysql205.phy.lolipop.lan'
+# user='LAA1281735'
+# passwd='Pa22wadoh'
+# db='LAA1281735-omiaidb'
