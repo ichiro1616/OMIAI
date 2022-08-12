a9af635e24194396a57257442c5cceb6b9d761a0
yamato0502 <19206@toba-cmt.ac.jp>
2022-08-12 13:19:58 +0900

    微修正

--- PHP/simulation.php
+++ PHP/simulation.php
@@ -9,13 +9,13 @@ $result = array();
 // $result2 = array();
 
 try {
-$dbh = new PDO($dsn, $user, $password);
+    $dbh = new PDO($dsn, $user, $password);
 
-// 最新の2550個のレコードを取得
-$sql = "SELECT * FROM `lr.coef_` ORDER BY `lr.coef_id` DESC LIMIT 255";
-$stmt = $dbh->prepare($sql);
-$stmt->execute();
-$result = $stmt->fetchAll();
+    // 最新の2550個のレコードを取得
+    $sql = "SELECT * FROM `lr.coef_` ORDER BY `lr.coef_id` DESC LIMIT 255";
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
 
 
 // 指定したid以外で、指定したball_idと同じball_idのid、名前などの値をランダムで取得
@@ -25,8 +25,8 @@ $result = $stmt->fetchAll();
 // $result2 = $stmt2->fetchAll();
 // $result_arr = array($result, $result2);
 } catch (Exception$e) {
-echo$e->getMessage();
-exit();
+    echo$e->getMessage();
+    exit();
 }
 
 header('Content-type: application/json');
