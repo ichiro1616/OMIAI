0eb3bf9b7dfded3f237f22674cca1a3f1ed8c7b6
Minerva <19205@toba.kosen-ac.jp>
2022-09-04 12:42:12 +0900

    途中　うえむら

--- PHP/movie_receive.php
+++ PHP/movie_receive.php
@@ -14,13 +14,12 @@
 $experience_years = $_POST["experience_years"];
 include 'db_config.php';
 
-$cate_num = 584; //動画の総数
-$DATA = array(); //registerテーブルから取得したデータ
-$data = array(); //movieテーブルから取得したデータ
-$count = array(); //movie_categorizeそれぞれについての回答数
-$COUNT = array(); //回答数が最も少ないmovie_categorizeのkeyを格納する。
-$c = array(); //回答数が最も少ない動画の中から5つを抽出する。
-$prev = array(); //一つ前のmovie_id
+$cate_num = 584; //left_or_right = -1 のmovie_idの総数
+$result = array(); //該当のmovie_id
+$prev = array(); //resultの一つ前のmovie_id
+$data = array(); //該当のmovie_idの動画データ
+$DATA = array(); //resultの一つ前のmovie_idの動画データ
+$sendDATA = array(); //dataとDATAをまとめた配列
 
 try{
     $dbh = new PDO($dsn, $user, $password);
@@ -33,47 +32,67 @@ try{
     $STMT = $dbh->query($sql);
     $_DATA = $STMT->fetchAll(PDO::FETCH_ASSOC);
 
-    //回答数が少ない順に100球分のmovie_idを取り出す
-    for($j=0; $j<100; $j++){
-        $TMP = $_DATA[$j]['movie_id'];
-        $DATA[] = $TMP;
-    }
-
-    //取り出したmovie_idから-1した値の配列をつくる
+//--------------------------------------------left_or_right = -1 のmovie_idの取り出し----------------------------------------------------------
     for($h=0; $h<100; $h++){
-        $temp = $DATA[$h];
-        $prev[] = $temp;
+        $TMP = $_DATA[$h]['movie_id'];
+        $result[] = $TMP;
     }
+    for($i=0;$i<100;$i++){
+        $sql = sprintf("SELECT `movie_id`,`movie_categorize`, `stop_time`, `movie_path` FROM `movie` WHERE movie_id = '%s';", $result[$i]);
+        $stmt = $dbh->query($sql);
+        $_data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
-// while(count($c)<5){
-//     $ppp = count(array_keys($count,min($count)));
-//     for($i = 0; $i <$ppp; $i++){
-//         array_push($c,array_keys($count,min($count))[$i]);
-//     }
-//     for($i = 0; $i < count($c); $i++){
-//         $count[$c[$i]] = 100000;
-//     }
-// }
-
-//回答がある場合は回答が特に少ない順に3つの動画を取り出す
-for($i=0;$i<5;$i++){
-    $sql = sprintf("SELECT `movie_id`,`movie_categorize`, `stop_time`, `movie_path`, `left_player_id`, `right_player_id` FROM `movie` WHERE movie_categorize = '%s';", $c[$i]+1);
-    $stmt = $dbh->query($sql);
-    $_data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    foreach($_data as $d){
-        $tmp = array(
-            "movie_id" => $d['movie_id'],
-            "movie_categorize" => $d['movie_categorize'],
-            "stop_time" => $d['stop_time'],
-            "movie_path" => $d['movie_path'],
-            "left_player_id" => $d['left_player_id'],
-            "right_player_id" => $d['right_player_id']
-        );
-        $data[]=$tmp;
+        foreach($_data as $d){
+            $tmp = array(
+                "movie_id" => $d['movie_id'],
+                "movie_categorize" => $d['movie_categorize'],
+                "stop_time" => $d['stop_time'],
+                "movie_path" => $d['movie_path'],
+            );
+            $data[]=$tmp;
+        }
     }
-}
+//---------------------------------------left_or_right = -1 のmovie_idの1つ前のmovie_id取り出し-------------------------------------------------
+    for($j=0; $j<100; $j++){
+        $temp = $result[$j];
+        $prev[] = $temp - 1;
+    }
+    for($k=0;$k<100;$k++){
+        $sql = sprintf("SELECT `movie_id`,`movie_categorize`, `stop_time`, `movie_path` FROM `movie` WHERE movie_id = '%s';", $prev[$k]);
+        $stmt = $dbh->query($sql);
+        $_DATA = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        for($m=0;$m<100;$m++){
+            //-1した結果movie_id=0になってしまった場合はmovie_id=1の最初から再生を行う
+            if($_DATA[$m]['movie_id'] == 0){
+                $TMP = array(
+                    "movie_id" => 1,
+                    "movie_categorize" => $data[$m]['movie_categorize'],
+                    "start_time" => 0,
+                    "movie_path" => $data[$m]['movie_path'],
+                );
+            }
+            //動画が切り替わってしまった場合は、切り替わった後の動画の最初から再生を行う
+            elseif($_DATA[$m]['movie_categorize'] != $data[$m]['movie_categorize']){
+                $TMP = array(
+                    "movie_id" => $data[$m]['movie_id'],
+                    "movie_categorize" => $data[$m]['movie_categorize'],
+                    "start_time" => 0,
+                    "movie_path" => $data[$m]['movie_path'],
+                );
+            }
+            //
+            else{
+                $TMP = array(
+                    "movie_id" => $_DATA[$m]['movie_id'],
+                    "movie_categorize" => $_DATA[$m]['movie_categorize'],
+                    "start_time" => $_DATA[$m]['stop_time'],
+                    "movie_path" => $_DATA[$m]['movie_path'],
+                );
+            }
+            $DATA[]=$tmp;
+        }
+    }
 
 }catch(PDOException $e){
     print('Error:' .$e->getMessage());
