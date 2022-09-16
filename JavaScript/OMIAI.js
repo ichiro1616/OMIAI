a4a1a8454652e5b8002f21906c0c5e83a9eb77ef
kento <20243@toba.kosen-ac.jp>
2022-09-16 20:12:18 +0900

    ホーム画面にリアルタイムで状況を表示

---
+++ JavaScript/OMIAI.js
@@ -0,0 +1,30 @@
+let answer ; //客観的クイズ回答総数
+let simu; //配置登録総数
+let colle; //最大世代数
+window.addEventListener('DOMContentLoaded',() => {
+    total();
+    setInterval(() => {
+        total();
+    },3000);
+});
+function total() {
+    form = new FormData();
+    xhr = new XMLHttpRequest();
+    xhr.open("GET", "/PHP/OMIAI.php");
+    xhr.addEventListener("loadend", function () {
+        if (xhr.status == 200) {
+            let data = JSON.parse(xhr.response);
+            console.log(data)
+            answer = data[0];
+            simu = data[1];
+            colle = data[2];
+            document.getElementById('answer').innerHTML = "累計回答数:" + answer;
+            document.getElementById('simu').innerHTML = "累計登録数:"+(Math.floor(simu));
+            document.getElementById('colle').innerHTML = "最大世代数" + colle;
+            if (xhr.response == "error") {
+                console.log("通信に失敗しました。")
+            }
+        }
+    });
+    xhr.send(form)
+}
---
+++ PHP/OMIAI.php
@@ -0,0 +1,20 @@
+<?php
+include 'db_config.php';
+try{
+    $dbh = new PDO($dsn,$user,$password);
+    $stmt1 = $dbh->query("SELECT MAX(`answer_id`) FROM `answer`");
+    $data1 = $stmt1->fetchAll();
+    $stmt2 = $dbh->query("SELECT MAX(`register_id`) FROM `register`");
+    $data2 = $stmt2->fetchAll();
+    $stmt3 = $dbh->query("SELECT MAX(`generation`)  FROM `collective`");
+    $data3 = $stmt3->fetchAll();
+
+    $result = [$data1[0][0],$data2[0][0]/6,$data3[0][0]];
+}catch(PDOException $e){
+    print('Error:' .$e->getMessage());
+    die();
+}
+$dbh = null;
+header('Content-type: application/json');
+echo json_encode($result,JSON_UNESCAPED_UNICODE);
+?>
