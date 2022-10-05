f870f68173a09b0c9b27b1c49de7d04a3c7cd720
ichiro1616 <ichiro.nakagawa1616@gmail.com>
2022-10-05 12:50:57 +0900

    ボタン文字修正

--- JavaScript/OMIAI.js
+++ JavaScript/OMIAI.js
@@ -29,12 +29,12 @@ let judge = 1;
 function preview() {
   if (judge == 0) {
     document.getElementById("answer").innerHTML = "観客クイズ";
-    document.getElementById("simu").innerHTML = "配置シュミレーション";
+    document.getElementById("simu").innerHTML = "配置シミュレーション";
     document.getElementById("colle").innerHTML = "集合知との比較";
     judge = 1;
   } else {
-    document.getElementById("answer").innerHTML = "累計回答数:" + answer;
-    document.getElementById("simu").innerHTML = "累計登録数:" + Math.floor(simu);
+    document.getElementById("answer").innerHTML = "判断回答数:" + answer;
+    document.getElementById("simu").innerHTML = "配置登録数:" + Math.floor(simu);
     document.getElementById("colle").innerHTML = "最大世代数:" + Math.floor(colle);
     judge = 0;
   }
