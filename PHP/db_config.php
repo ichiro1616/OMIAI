49b4f87398b1783b3b08d0b43a87a36f5b7c63b0
yamato0502 <19206@toba-cmt.ac.jp>
2022-10-06 19:12:38 +0900

    コメントを修正

--- PHP/db_config.php
+++ PHP/db_config.php
@@ -1,10 +1,10 @@
 <?php
-//ローカル
+// ローカル
 $dsn = 'mysql:dbname=omiai_db;host=localhost';
 $user = 'root';
 $password = 'Pa22wadoh';
 
-//ロリポップ
+// ロリポップ
 // $dsn = 'mysql:dbname=LAA1281735-omiaidb;host=mysql205.phy.lolipop.lan';
 // $user = 'LAA1281735';
 // $password = 'Pa22wadoh';
